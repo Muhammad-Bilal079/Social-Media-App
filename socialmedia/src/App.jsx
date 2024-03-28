d8a87892d10a79cac3738df030fba530f6bd5c05
Muhammad-Bilal079 <mb4462087@gmail.com>
2024-03-29 01:22:10 +0500

    mern social media project

--- socialmedia/src/App.jsx
+++ socialmedia/src/App.jsx
@@ -1,10 +1,16 @@
 import Home from './pages/Home/home.jsx';
+import Login from './pages/login/Login.jsx';
+import Profile from './pages/profile/Profile.jsx';
+import Register from './pages/register/Register.jsx';
 
 function App() {
  
   return (
     <>
  <Home />
+ {/* <Profile/> */}
+ {/* <Login/> */}
+ {/* <Register/> */}
 
     </>
   )
--- socialmedia/src/components/rightbar/Right.jsx
+++ socialmedia/src/components/rightbar/Right.jsx
@@ -3,12 +3,12 @@ import "./right.css"
 import {Users} from '../../dummyData'
 import Online from '../online/Online'
 
-function Right() {
-  return (
-    <div className='rightbar'>
+function Right({profile}) {
 
-      <div className="rightbarWrapper">
-        <div className="birthdayContainer">
+  const Homerightbar = () => {
+    return (
+      <>
+      <div className="birthdayContainer">
           <img src="assets/gift.png" alt="" className="birthdayImg" />
           <span className="birthdayText"><b>sufyan</b> and <b>3 others have a birthday today</b></span>
         </div>
@@ -19,10 +19,74 @@ function Right() {
             <Online  key={u.id} user={u}/>
           ))}
          
-
-          
-
         </ul>
+      </>
+    )
+  }
+
+  // profile right bar 
+  const Profilerightbar = () =>{
+    return(
+      <>
+      <div className="rightbarTitle">User information</div>
+      <div className="rightbarInfo">
+
+        <div className="rightbarInfoItem">
+        <span className="rightbarInfoKey">City : </span>
+        <span className="rightbarInfoValue">NewYork</span>
+        </div>
+
+        <div className="rightbarInfoItem">
+        <span className="rightbarInfoKey">From : </span>
+        <span className="rightbarInfoValue">bilal</span>
+        </div>
+
+        <div className="rightbarInfoItem">
+        <span className="rightbarInfoKey">Relationship : </span>
+        <span className="rightbarInfoValue">Single</span>
+        </div>
+      </div>
+      {/* user friends  */}
+      <div className="rightbarTitle">User Friends</div>
+      <div className="rightbarFollowings">
+        <div className="rightbarFollowing">
+        <img src="assets/person/1.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">John Carter</span>
+        </div>
+
+        <div className="rightbarFollowing">
+        <img src="assets/person/2.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">John Carter</span>
+        </div>
+
+        <div className="rightbarFollowing">
+        <img src="assets/person/9.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">John Carter</span>
+        </div>
+
+        <div className="rightbarFollowing">
+        <img src="assets/person/4.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">John Carter</span>
+        </div>
+
+        <div className="rightbarFollowing">
+        <img src="assets/person/5.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">John Carter</span>
+        </div>
+
+        <div className="rightbarFollowing">
+        <img src="assets/person/8.jpeg" alt="" className="rightbarFollowingImg" />
+        <span className="rightbarFollowingName">John Carter</span>
+        </div>
+      </div>
+      </>
+    )
+  }
+  return (
+    <div className='rightbar'>
+      <div className="rightbarWrapper">
+        {/* <Profilerightbar/> */}
+        {profile ?<Profilerightbar/> : <Homerightbar/> }
       </div>
 
     </div>
